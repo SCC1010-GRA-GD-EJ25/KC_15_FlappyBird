44bb806873ecc03e757f0a5f4c414794ed9e966c
Kitzia Marisol Crespo Hernández <L23171148@culiacan.tecnm.mx>
2025-03-12 22:56:42 +0000

    Se agregaron obstáculos

--- sketch.js
+++ sketch.js
@@ -1,16 +1,25 @@
 let imagenFondo;
 let imagenInicio;
 let personaje;
+let pared;
 let x = 0;
 let posY = 100;
 let dY = 3;
 let estado = 0; // 0: menú; 1: jugando; 2: gameOver
-
+let wallX = []
+let wallY = []
+let puntaje = 0;
+let puntajeMax = 0;
+let recordAnterior = 0;
 
 function preload() {
   // put preload code here
-  imagenFondo = loadImage('./images/fondojuego00.png');
+  // imagenFondo = loadImage('./images/fondojuego00.png');
+  // imagenFondo = loadImage('./images/fondojuego01.jpg');
+  imagenFondo = loadImage('./images/fondojuego02.jpg');
+  imagenInicio = loadImage('./images/fondoinicio00.jfif');
   personaje = loadImage('./images/bird.png');
+  pared = loadImage('./images/pared.png');
 }
 
 function setup() {
@@ -23,6 +32,7 @@ function setup() {
 function draw() {
   // put drawing code here
   if (estado === 1) {
+    imageMode(CORNER);
     background(255)
     image(imagenFondo, x, 0);
     image(imagenFondo, x + imagenFondo.width, 0);
@@ -34,14 +44,40 @@ function draw() {
       x = 0;
     }
 
+      // Obstáculos
+    for (let i = 0; i < wallX.length; i++) {
+      imageMode(CENTER);
+      image(pared, wallX[i], wallY[i] - 500);
+      image(pared, wallX[i], wallY[i] + 500);
+
+      if (wallX[i] < 0) {
+        wallX[i] = width; // Lo envía al final de la pantalla
+        wallY[i] = random(200, 300);
+      }
+
+      // Puntaje
+      if (wallX[i] === 100) {
+        puntaje = puntaje + 1;
+        puntajeMax = max(puntaje, puntajeMax);
+      }
+      wallX[i] = wallX[i] - 5; // Para que se muevan los obstáculos
+    }
+
+    // Personaje
     image(personaje, 100, posY, 60, 60);
+    text("Puntaje: " + puntaje, width/2-60, 30);
   } else if (estado === 0) {
     cursor();
-    image(imagenFondo, 0, 0);
-    textSize(50);
+    image(imagenInicio, 0, 0, 850, 600);
+    textSize(40);
     fill(255);
-    text("Haga clic para comenzar", 100, 100);
+    text("Puntaje Máximo: ", 300, 100);
+    text("Haga clic para comenzar", 300, 200);
+    textSize(20);
+    text("ola hannia ❤", 200, 200);
   }
+
+
 
 }
 
@@ -52,6 +88,13 @@ function mousePressed() {
     posY = 100;
     x = 0;
     dY = 3;
+    wallX = [500, 800, 1100]
+    wallY[0] = random(200, 300)
+    wallY[1] = random(200, 300)
+    wallY[2] = random(200, 300)
+    puntaje = 0;
+    recordAnterior = puntajeMax;
+
     noCursor();
   }
   dY  = -15;
